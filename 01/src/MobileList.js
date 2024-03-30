40bc2f1e45c29439fec05d5443d5c37489b9a2e5
satyamrao789 <satyam987987@gmail.com>
2024-03-30 13:27:29 +0530

    modified mobileList

--- 01/src/MobileList.js
+++ 01/src/MobileList.js
@@ -2,14 +2,18 @@ import React from 'react'
 import './MobileList.css'
 
 export default function MobileList() {
+
+    const image = 'https://rukminim2.flixcart.com/image/312/312/l3rmzrk0/mobile/v/f/y/-original-imagetmeguhrdsqs.jpeg?q=70';
+    const productTitle = 'Realme GT Master Edition';
+    const price = 90;
+
     return (
         <div className='mainTag'>
-            <img className='productImage' src='https://rukminim2.flixcart.com/image/312/312/l3rmzrk0/mobile/v/f/y/-original-imagetmeguhrdsqs.jpeg?q=70' />
-                <h2>Realme GT Master Edition</h2>
-            
-                <p>$ 90</p>
-                <button>Add to Cart</button>
-            
+            <img className='productImage' src={image} />
+            <h2>{productTitle}</h2>
+            <p>$ {price}</p>
+            <button>Add to Cart</button>
+
         </div>
     )
 }
