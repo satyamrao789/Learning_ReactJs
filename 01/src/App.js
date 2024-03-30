0608affef049e494196a7f2bea53dae9095cfff3
satyamrao789 <satyam987987@gmail.com>
2024-03-30 12:44:02 +0530

    created App.css

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,9 +1,11 @@
+import './App.css';
+
 function App() {
     let date = 31;
     let month = "March";
     return (
         <div>
-            <h1>Today is {date}  {month}</h1>
+            <h1 className='main'>Today is {date}  {month}</h1>
             <h1>Hello I am App component</h1>
             <p>Hello I am a paragraph</p>
         </div>
