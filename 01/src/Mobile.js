44ee9d2af22c9ec18a454cc385f142d38d460cc4
satyamrao789 <satyam987987@gmail.com>
2024-03-30 13:45:13 +0530

    intro to props

--- 01/src/Mobile.js
+++ 01/src/Mobile.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import MobileList from './MobileList';
 
+const item = {
+    image: "https://rukminim2.flixcart.com/image/312/312/l3rmzrk0/mobile/v/f/y/-original-imagetmeguhrdsqs.jpeg?q=70",
+    productTitle: "Realme GT Master Edition",
+    price: 90
+}
+
 export default function Mobile() {
-  return (
-    <div>
-    <h1>Checkout the list of Mobiles available</h1>
-    <MobileList/>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Checkout the list of Mobiles available</h1>
+            <MobileList image={item.image} productTitle={item.productTitle} price={item.price} />
+        </div>
+    )
 }
--- 01/src/MobileList.js
+++ 01/src/MobileList.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './MobileList.css'
 
-export default function MobileList() {
+export default function MobileList(props) {
 
-    const item = {
-        image: "https://rukminim2.flixcart.com/image/312/312/l3rmzrk0/mobile/v/f/y/-original-imagetmeguhrdsqs.jpeg?q=70",
-        productTitle: "Realme GT Master Edition",
-        price: 90
-    }
+    // const item = {
+    //     image: "https://rukminim2.flixcart.com/image/312/312/l3rmzrk0/mobile/v/f/y/-original-imagetmeguhrdsqs.jpeg?q=70",
+    //     productTitle: "Realme GT Master Edition",
+    //     price: 90
+    // }
 
     // const image = 'https://rukminim2.flixcart.com/image/312/312/l3rmzrk0/mobile/v/f/y/-original-imagetmeguhrdsqs.jpeg?q=70';
     // const productTitle = 'Realme GT Master Edition';
@@ -15,9 +15,9 @@ export default function MobileList() {
 
     return (
         <div className='mainTag'>
-            <img className='productImage' src={item.image} />
-            <h2>{item.productTitle}</h2>
-            <p>$ {item.price}</p>
+            <img className='productImage' src={props.image} />
+            <h2>{props.productTitle}</h2>
+            <p>$ {props.price}</p>
             <button>Add to Cart</button>
 
         </div>
