c9fb40961ff4cb159e0a89bff86653c60b0800bb
satyamrao789 <satyam987987@gmail.com>
2024-03-30 13:29:48 +0530

    created item object in MobileList.js

--- 01/src/MobileList.js
+++ 01/src/MobileList.js
@@ -3,15 +3,21 @@ import './MobileList.css'
 
 export default function MobileList() {
 
-    const image = 'https://rukminim2.flixcart.com/image/312/312/l3rmzrk0/mobile/v/f/y/-original-imagetmeguhrdsqs.jpeg?q=70';
-    const productTitle = 'Realme GT Master Edition';
-    const price = 90;
+    const item = {
+        image: "https://rukminim2.flixcart.com/image/312/312/l3rmzrk0/mobile/v/f/y/-original-imagetmeguhrdsqs.jpeg?q=70",
+        productTitle: "Realme GT Master Edition",
+        price: 90
+    }
+
+    // const image = 'https://rukminim2.flixcart.com/image/312/312/l3rmzrk0/mobile/v/f/y/-original-imagetmeguhrdsqs.jpeg?q=70';
+    // const productTitle = 'Realme GT Master Edition';
+    // const price = 90;
 
     return (
         <div className='mainTag'>
-            <img className='productImage' src={image} />
-            <h2>{productTitle}</h2>
-            <p>$ {price}</p>
+            <img className='productImage' src={item.image} />
+            <h2>{item.productTitle}</h2>
+            <p>$ {item.price}</p>
             <button>Add to Cart</button>
 
         </div>
