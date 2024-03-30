26dabfef671a4bb1b200431c9e25958499949128
satyamrao789 <satyam987987@gmail.com>
2024-03-30 22:03:52 +0530

    useState example 2 remove last value

--- 01/src/Profile.js
+++ 01/src/Profile.js
@@ -11,6 +11,13 @@ const Profile = () => {
         setnData([]); // sets the nData as empty
     }
 
+    //handleRemoveLast() - removes last value from array.
+    const handleRemoveLast = () => {
+        const newData = [...nData]; // used spread(...) operator to make shallow copy of nData in newData
+        newData.pop();
+        setnData(newData); // update the state with the new array containing the last element removed
+    }
+
     return (
         <div className='mainTag'>
             <ol>
@@ -23,6 +30,7 @@ const Profile = () => {
                 }
             </ol>
             <button onClick={handleRemoveAll}>Clear ALL</button>
+            <button onClick={handleRemoveLast}>Remove Last Value</button>
            
         </div>
     )
