a9c6a970040ea0a6ac510e2fdba8ecd94f4cc5c7
satyamrao789 <satyam987987@gmail.com>
2024-03-30 12:51:24 +0530

    inline styling

--- 01/src/App.js
+++ 01/src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
         <div>
             <h1 className='main'>Today is {date}  {month}</h1>
-            <h1>Hello I am App component</h1>
+            <h1 style={{backgroundColor:"aqua",border: "2px solid yellow"}}>Hello I am App component</h1>
             <p>Hello I am a paragraph</p>
         </div>
     )
